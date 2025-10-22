9617f2540af2e1106cbfe247469ffc116d46b4c0
maria-lopezc <maria.lopezcaceres1@gmail.com>
2025-10-21 23:56:44 -0300

    cambios en bbdd

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-10-2025 a las 04:34:41
+-- Tiempo de generación: 22-10-2025 a las 04:42:46
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `autores` (
-  `id_autor` int(11) NOT NULL,
+  `id_autor` int(11)  AUTO_INCREMENT PRIMARY KEY,
   `nombre` varchar(100) NOT NULL,
   `nacimiento` date NOT NULL,
   `email` varchar(50) NOT NULL
@@ -42,7 +42,8 @@ INSERT INTO `autores` (`id_autor`, `nombre`, `nacimiento`, `email`) VALUES
 (1, 'Luciano Añon', '2004-08-03', 'luciano@gmail.com'),
 (2, 'Julio Verne', '2000-01-01', 'julioverne32@gmail.com'),
 (3, 'Jose Perez', '1995-06-21', 'joseperez@gmail.com'),
-(4, 'Juana Rodriguez', '1999-03-08', 'juanarodriguez@gmail.com');
+(4, 'Juana Rodriguez', '1999-03-08', 'juanarodriguez@gmail.com'),
+(5, 'Emma', '2025-10-15', '20.emma.lop@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -51,7 +52,7 @@ INSERT INTO `autores` (`id_autor`, `nombre`, `nacimiento`, `email`) VALUES
 --
 
 CREATE TABLE `libros` (
-  `id_libro` int(11) NOT NULL,
+  `id_libro` int(11)  AUTO_INCREMENT PRIMARY KEY,
   `id_autor` int(11) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `genero` varchar(100) NOT NULL,
@@ -78,7 +79,7 @@ INSERT INTO `libros` (`id_libro`, `id_autor`, `titulo`, `genero`, `paginas`) VAL
 --
 
 CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
+  `id` int(11)  AUTO_INCREMENT PRIMARY KEY,
   `usuario` varchar(100) CHARACTER SET utf16 COLLATE utf16_spanish_ci NOT NULL,
   `contrasena` varchar(200) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
@@ -114,13 +115,13 @@ ALTER TABLE `libros`
 -- AUTO_INCREMENT de la tabla `autores`
 --
 ALTER TABLE `autores`
-  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
